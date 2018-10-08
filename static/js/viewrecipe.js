f0ca78cb3fe0aa7872db0de7fcc53e16ba219f26
John L <johnlonggately@gmail.com>
2018-10-08 17:27:43 +0000

    added javascript to file so menu works on viewrecipe page

---
+++ static/js/viewrecipe.js
@@ -0,0 +1,17 @@
+$('#burger').click(function(){
+    if($('.nav-items').css('display') === 'none'){
+        $('.nav-items').css('display','block');
+    } else {
+        $('.nav-items').css('display','none');
+    }
+});
+
+// make it so menu items display on large screens if menu has been closed on small screen
+$( window ).resize(function() {
+  if($(window).width() > 767) {
+       $('header').css('display', 'grid');
+	   $('.nav-items').css('display','block');
+  } else {
+      $('.nav-items').css('display','none');
+  }
+});
