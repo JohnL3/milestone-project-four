7fb96b2f5c0a022dca54f317da474d3b424db918
John L <johnlonggately@gmail.com>
2018-10-08 17:49:38 +0000

    added javascript to the viewrecipe.js file for clicks on a view changer on mobile sizes

--- static/js/viewrecipe.js
+++ static/js/viewrecipe.js
@@ -15,3 +15,41 @@ $( window ).resize(function() {
       $('.nav-items').css('display','none');
   }
 });
+
+$('.li-first').click(function(){
+    
+   if($(this).css('background-color') === 'rgb(240, 128, 128)' && $(window).width() < 768) {
+       $(this).css('background-color', '#f7bfbf');
+       $(this).css('color', 'darkslategray');
+       $('.li-second').css('background-color','#f08080');
+       $('.li-second').css('color','white');
+       $('.display-method').css('display', 'block');
+        $('.display-ing').css('display', 'none');
+   } else {
+       $(this).css('background-color', '#f08080');
+       $('.li-second').css('background-color','#f7bfbf');
+       $(this).css('color', 'white');
+       $('.li-second').css('color','darkslategray');
+        $('.display-method').css('display', 'none');
+        $('.display-ing').css('display', 'block');
+   }
+});
+
+$('.li-second').click(function(){
+   
+    if($(this).css('background-color') === 'rgb(247, 191, 191)' && $(window).width() < 768) {
+       $(this).css('background-color', '#f08080');
+        $(this).css('color', 'white');
+       $('.li-first').css('background-color','#f7bfbf');
+       $('.li-first').css('color','darkslategray');
+       $('.display-method').css('display', 'block');
+       $('.display-ing').css('display', 'none');
+   } else {
+       $(this).css('background-color', '#f7bfbf');
+         $('.li-first').css('background-color','#f08080');
+         $(this).css('color', 'darkslategray');
+         $('.li-first').css('color','white');
+         $('.display-method').css('display', 'none');
+         $('.display-ing').css('display', 'block');
+   }
+});
