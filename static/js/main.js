23fca2b66e32afc522538ebdb81f0582fdf73d07
John L <johnlonggately@gmail.com>
2018-10-08 15:38:55 +0000

    added the javascrpt code to start makeing filters function

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,60 @@ $( window ).resize(function() {
       $('.nav-items').css('display','none');
   }
 });
+
+/************************* Recipe Page Filter *********************************************/
+
+$('.filters-header').click(function(){
+  if($('.filter-items-con').css('display') === 'none') {
+  
+    $('.filter-items-con').css('display','block');
+    
+    if($(window).width() < 768) {
+        $('nav').css('display','none');
+        $('header').css('display','none');
+    }
+  } else {
+      if($(window).width() > 767){
+        $('nav').css('display','inline-block');
+        $('header').css('display','grid');
+      } else {
+        $('nav').css('display','block');
+        $('header').css('display','block');
+      }
+    $('.filter-items').css('display','block');
+
+    $('.filter-items-con').css('display','none');
+    $('.type').css('display','none');
+  }
+});
+
+$('.author').click(function(){
+  if($('.author-input').css('display') === 'none') {
+    $('.author-input').css('display','block');
+    $('.category-input').css('display','none');
+    $('.allergen-con').css('display','none');
+  } else {
+    $('.author-input').css('display','none');
+  }
+});
+
+$('.category').click(function(){
+  if($('.category-input').css('display') === 'none') {
+    $('.category-input').css('display','block');
+    $('.author-input').css('display','none');
+    $('.allergen-con').css('display','none');
+  } else {
+    $('.category-input').css('display','none');
+  }
+});
+
+$('.allergen').click(function(){
+  if($('.allergen-con').css('display') === 'none') {
+    $('.allergen-con').css('display','block');
+    $('.category-input').css('display','none');
+    $('.author-input').css('display','none');
+  } else {
+    $('.allergen-con').css('display','none');
+  }
+});
+
