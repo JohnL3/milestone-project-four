17261e7ad0579e5c64103d80482cce4e70e01f92
John L <johnlonggately@gmail.com>
2018-10-18 10:53:46 +0000

    created function to add recipe to database

--- database/DB_functions.py
+++ database/DB_functions.py
@@ -58,4 +58,102 @@ def validate_user(mysql, username, password):
     else:
         return False
         
+def create_new_recipe(mysql, data):
+    con = mysql.connect()
+    curs = con.cursor()
+    
+    username = data['username']
+    category = data['category_name']
+    
+    query = "SELECT user_id FROM user_table WHERE user_name ='" +username+ "'"
+    curs.execute(query)
+    user_id = curs.fetchall()
+    user_id = user_id[0]['user_id']
+    
+    query = "SELECT author_id FROM author_table WHERE author_name ='" +data['author_name']+ "'"
+    curs.execute(query)
+    author_id = curs.fetchall()
+    if len(author_id) == 0:
+        try:
+            name = data['author_name']
+            curs.execute('''INSERT INTO author_table (author_id, author_name) VALUES (NULL, %s)''', name)
+            con.commit()
+        except Exception as e:
+            return 'Author section Error saving data: '+str(e)
+        
+        curs.execute('''SELECT LAST_INSERT_ID()''')
+        author_id = curs.fetchall()
+        author_id = author_id[0]['LAST_INSERT_ID()']
+    else:
+        author_id = author_id[0]['author_id']
+    
+    query = "SELECT cat_id FROM category_table WHERE category_name ='" +category+ "'"
+    curs.execute(query)
+    cat_id = curs.fetchall()
+    
+    if len(cat_id) == 0:
+        try:
+            name = data['category_name']
+            curs.execute('''INSERT INTO category_table (cat_id, category_name) VALUES (NULL, %s)''', name)
+            con.commit()
+        except Exception as e:
+            return 'Category section Error saving data: '+str(e)
+        
+        curs.execute('''SELECT LAST_INSERT_ID()''')
+        cat_id = curs.fetchall()
+        cat_id = cat_id[0]['LAST_INSERT_ID()']
+    else:
+        cat_id = cat_id[0]['cat_id']
+	
+	
+    Egg = data['allergens'][0]
+    Moll = data['allergens'][1]
+    Pnuts = data['allergens'][2]
+    Nuts = data['allergens'][3]
+    SBeans = data['allergens'][4]
+    Milk = data['allergens'][5]
+    Celery = data['allergens'][6]
+    Mustard = data['allergens'][7]
+    SSeeds = data['allergens'][8]
+    Fish = data['allergens'][9]
+    Lupin = data['allergens'][10]
+    SDioxide = data['allergens'][11]
+    Cerals = data['allergens'][12]
+    Crust = data['allergens'][13]
+    
+    ins = data['instructions']
+    prep = data['prep']
+    cook = data['cook']
+    serves = data['serves']
+    url = data['url']
+    recipe_name = data['recipe_name']
+    
+    
+    try:
+        row = (recipe_name,Nuts,Egg,Milk,Pnuts,Celery,Mustard,SSeeds,Fish,Moll,SBeans,Lupin,SDioxide,Cerals,Crust, ins, 0, 0, prep, cook, serves, url, cat_id, author_id, user_id)
+        curs.execute('''INSERT INTO recipe_table(recipe_id, recipe_name, Nuts, Egg, Milk, Pnuts, Celery, Mustard, SSeeds, Fish, Moll, SBeans, Lupin, SDioxide, Cerals, Crust, instructions, likes, collected, prep, cook, serves, url, cat_id, author_id, user_id) VALUES(NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',row)
+        con.commit()
+    except Exception as e:
+        return 'Recipe section Error saving data: '+str(e)
+        
+    
+    try:
+        rows = data['ing_and_quan']
+        curs.executemany('''INSERT INTO ingredients_table(recipe_id, ing_name, ing_quantity) VALUES(LAST_INSERT_ID(), %s, %s)''', rows)
+        con.commit()
+    except Exception as e:
+        return ' Ing sect Error saving data: '+str(e)
+            
+           
+        
+  
+    
+    
+    
+    
+    
+    
+    
+    
+        
     
