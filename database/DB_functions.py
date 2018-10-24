aca3eeb184bda08cef7955647642807ad6273ddf
John L <johnlonggately@gmail.com>
2018-10-24 17:41:33 +0000

    added helper function for filter_all_recipes and tested to make sure first section works ok

--- database/DB_functions.py
+++ database/DB_functions.py
@@ -325,11 +325,26 @@ def check_what_to_filter_by(data):
         filter_by = list(data)
         return filter_by
 
+def filter_by_category(mysql, filter_type, val):
+    con = mysql.connect()
+    curs = con.cursor()
+    
+    start_str = get_query_string('all')
+    end_str = " where "+filter_type+" = '"+val+"' ORDER BY author_name ASC;"
+    sql = start_str+end_str
+    
+    curs.execute(sql)
+    return curs.fetchall()
 
 def filter_all_recipes(mysql,data):
     items = check_what_to_filter_by(data)
     
-    return items
+    if len(items) == 1:
+        if 'author_name' in items or 'category_name' in items:
+            result = filter_by_category(mysql, items[0], data[items[0]])
+            return result
+    
+   
     
     
     
